abb450084dcad693954b9c2efd3f0b4ba58b7e8e
Dorian Kossowski <dorian.kossowski@codibly.com>
2022-03-21 21:51:04 +0100

    Fix error handling

--- src/helpers/ErrorHandlingService.js
+++ src/helpers/ErrorHandlingService.js
@@ -1,11 +1,13 @@
 const handle = (error, msg) => {
+  console.log(error);
+    if(error.status === 401) {
+        sessionStorage.clear();
+        return msg + 'Unauthorized'
+    }
+    if(error.status === 500) {
+        return msg + 'Interal error'
+    }
     if (error.data) {
-        if(error.status === 401) {
-            sessionStorage.clear();
-        }
-        if(error.status === 500) {
-            return msg + 'Interal error'
-        }
         return msg + error.data.message;
     } else {
         return msg + error;
